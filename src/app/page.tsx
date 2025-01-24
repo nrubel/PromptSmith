90d9abde588ae20505b1bf89e1610aeb6cbade24
Nasir Uddin <nais@bdfare.com>
2025-01-24 22:14:38 +0600

    Update layout width in Home component for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex justify-center items-center w-full min-h-screen">
       <div
         className={
-          "p-6 bg-white shadow-xl rounded-sm border border-collapse border-gray-100 w-5/12"
+          "p-6 bg-white shadow-xl rounded-sm border border-collapse border-gray-100 w-11/12 lg:w-7/12 xl:w-5/12"
         }
       >
         <PromptForm />
